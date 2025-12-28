08b7e22cf7f6d16370b6982ac0f861e50cf350ec
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-12-28 10:24:37 +0800

    Drawed Landmarks

--- VolumeGesture/main.py
+++ VolumeGesture/main.py
@@ -1,11 +1,15 @@
 import cv2 as cv
+import HandTrack as ht
 import time 
 
 
 capture = cv.VideoCapture(0)
+hand_tracker = ht.HandTracker()
 
 while True:
     successful, imgs = capture.read()
+
+    imgs = hand_tracker.hand_landmarker(imgs)
 
     if not successful:
         print('Error')
