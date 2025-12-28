acec3d0eeebf5650b7b9c950d3ead5f201ead46d
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-12-28 12:12:23 +0800

    Print Index Finger Tip

--- VolumeGesture/main.py
+++ VolumeGesture/main.py
@@ -10,11 +10,14 @@ while True:
     successful, imgs = capture.read()
 
     imgs = hand_tracker.hand_landmarker(imgs)
+    landmark_position = hand_tracker.landmark_position(imgs)
 
     if not successful:
         print('Error')
         break
 
+    if landmark_position:
+        print(landmark_position[8])
 
     cv.imshow('Live Video', imgs)
 
