e46d18d4c85e7c8774627ff1634a1c7d359620f3
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-12-29 11:17:05 +0800

    Added Frames per Second in the frame

--- VolumeGesture/main.py
+++ VolumeGesture/main.py
@@ -6,6 +6,9 @@ import time
 capture = cv.VideoCapture(0)
 hand_tracker = ht.HandTracker(num_hands=1, detection_confidence=0.8)
 
+p_time = 0
+c_time = 0
+
 while True:
     successful, imgs = capture.read()
 
@@ -26,6 +29,12 @@ while True:
         cv.line(imgs, (x1, y1), (x2, y2), (0, 255, 0), 3)
         cv.circle(imgs, (cx, cy), 8, (0, 255, 0), -1)
 
+
+    c_time = time.time()
+    fps = 1 / (c_time - p_time)
+    p_time = c_time
+
+    cv.putText(imgs, f'FPS : {int(fps)}', (30, 50), cv.FONT_HERSHEY_PLAIN, 2, (250, 0, 0), 3)
     cv.imshow('Live Video', imgs)
 
     if cv.waitKey(1) & 0xFF==27:
