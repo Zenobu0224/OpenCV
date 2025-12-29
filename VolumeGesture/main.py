175ac7a4b57ac89894a7dc5e54051d0d0cdc637c
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2025-12-29 12:21:02 +0800

    Added and set up pycaw for audio

--- VolumeGesture/main.py
+++ VolumeGesture/main.py
@@ -1,11 +1,15 @@
 import cv2 as cv
 import HandTrack as ht
 import numpy as np
+from pycaw.pycaw import AudioUtilities
 import time 
 
 
 capture = cv.VideoCapture(0)
 hand_tracker = ht.HandTracker(num_hands=1, detection_confidence=0.8)
+
+device = AudioUtilities.GetSpeakers()
+speaker_vol = device.EndpointVolume
 
 p_time = 0
 c_time = 0
@@ -44,6 +48,7 @@ while True:
             cv.line(imgs, (x1, y1), (x2, y2), (0, 0, 250), 3)
             cv.circle(imgs, (cx, cy), 8, (0, 0, 250), -1)
 
+    
 
     c_time = time.time()
     fps = 1 / (c_time - p_time)
