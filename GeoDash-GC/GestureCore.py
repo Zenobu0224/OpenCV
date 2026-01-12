333bfdefc911f0b2e1e9a77637b4b57a3b571569
Zenobu0224 <s.cwsbajura@usm.edu.ph>
2026-01-12 21:11:31 +0800

    Initialized the model

--- GeoDash-GC/GestureCore.py
+++ GeoDash-GC/GestureCore.py
@@ -8,3 +8,14 @@ class HandTrack():
         self.num_hands = num_hands
         self.detection_confidence = detection_confidence
         self.tracking_confidence = tracking_confidence
+
+        self.mp_hands = mp.solutions.hands
+        self.hands = self.mp_hands.Hands(static_image_mode=mode,
+                                         max_num_hands=num_hands,
+                                         min_detection_confidence=detection_confidence,
+                                         min_tracking_confidence=tracking_confidence)
+        self.mp_draw = mp.solutions.drawing_utils
+
+    # def draw_landmarks(imgs, draw=True):
+    #     img_rgb = cv.cvtColor(imgs, cv.COLOR_BGR2RGB)
+    #     self.results
